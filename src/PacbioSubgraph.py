ccea1ed47b6c9bbc689986c7720e0aa34607e958
dbrowneup <dbrowne.up@gmail.com>
2016-04-28 17:23:33 -0500

    Added test_connect method to PacbioSubgraph

--- src/PacbioSubgraph.py
+++ src/PacbioSubgraph.py
@@ -46,12 +46,16 @@ from illumina_graph import Graph
 from illumina_path import IlluminaPath
 from utils import toggleStrand
 import numpy as np
+import itertools
 
 class PacbioSubgraph(Graph):
 
     def __init__(self, pacbio_id, illumina_graph, pacbio_mapping):
         Graph.__init__(self)
         self.pacbio_id = pacbio_id
+        self.FivePOvhg = []
+        self.ThrePOvhg = []
+        self.Connects = []
         #Create array of read aligments by read length
         self.pacbio_mapping = pacbio_mapping
         self.readArray = self.read_stack()
@@ -64,19 +68,26 @@ class PacbioSubgraph(Graph):
             self.mark_coords(n, qStart, qEnd)
         #Calculate per-base coverage of read
         self.covArray = self.readArray.sum(axis=0)
+        print "Total coverage of read:", self.covArray.sum()
         #Filter out alignments in repetitive regions
         align_coords = set([])
         repeat_coords = set(self.find_repeats())
         for n in self.pacbio_mapping.readToContig[pacbio_id]:
             align_coords = set(range(n.qStart, n.qEnd))
             self.filter_repeats(align_coords, repeat_coords, n)
-        #Find 5' and 3' read overhangs
+        #Find 5' and 3' overhangs
         for n in self.pacbio_mapping.readToContig[pacbio_id]:
             self.find_overhangs(n)
+        #Connect 5' and 3' overhangs
+        possible_connects = list(itertools.product(self.ThrePOvhg, self.FivePOvhg))
+        self.Connects = map(self.test_connect, possible_connects)
+        
+    
     def read_stack(self):
         stack_size = len(self.pacbio_mapping.readToContig[self.pacbio_id])
         read_size = self.pacbio_mapping.readToContig[self.pacbio_id][0].qLength
         a = np.zeros((stack_size, read_size))
+        print "Shape of array:", np.shape(a)
         return a
     
     def mark_coords(self, n, qStart, qEnd):
@@ -88,9 +99,10 @@ class PacbioSubgraph(Graph):
         for i in range(len(self.covArray)):
             if self.covArray[i] > cutoff:
                 repeat_coords.append(i)
+        print str(len(repeat_coords))+" repetitive bps"
         return repeat_coords
 
-    def filter_repeats(self, align_coords, repeat_coords, n, fraction=0.7):
+    def filter_repeats(self, align_coords, repeat_coords, n, fraction=0.5):
         bp_in_repeat = align_coords.intersection(repeat_coords)
         alignment = self.pacbio_mapping.readToContig[self.pacbio_id]
         if len(bp_in_repeat) >= fraction*len(align_coords):
@@ -99,11 +111,28 @@ class PacbioSubgraph(Graph):
     def find_overhangs(self, a):
         if a.qStart > a.tStart and (a.qLength - a.qEnd) < (a.tLength - a.tEnd):
             print "5' overhang"
+            self.FivePOvhg.append(a)
         elif a.qStart < a.tStart and (a.qLength - a.qEnd) > (a.tLength - a.tEnd):
             print "3' overhang"
+            self.ThrePOvhg.append(a)
         elif a.qStart > a.tStart and (a.qLength - a.qEnd) > (a.tLength - a.tEnd):
             print "Double overhang"
         elif a.qStart <= a.tStart and (a.qLength - a.qEnd) <= (a.tLength - a.tEnd):
             print "No overhang"
         else:
             print "Unknown overhang!"
+    
+    def test_connect(self, p):
+        tp, fp = p
+        if tp.qEnd < fp.qStart:
+            if (tp.tLength - tp.tEnd + fp.tStart) < (fp.qStart - tp.qEnd):
+                print "Read spans gap between scaffolds"
+            else:
+                print "Gap between alignments but not scaffold ends"
+        elif fp.qEnd >= tp.qEnd >= fp.qStart >= tp.qStart:
+            if fp.tEnd < (fp.qEnd - tp.qStart) > (fp.tLength - fp.tStart):
+                print "Read spans overlap of scaffold ends"
+            else:
+                print "Scaffolds extend beyond alignment overlap"
+        else:
+            print "Strange alignment behavior"
