9e544c13df0dc2ec24fd5928fe65bc15399a5534
dbrowneup <dbrowne.up@gmail.com>
2016-05-06 04:40:13 -0500

    Small fix to PacbioSubgraph, wasn't properly removing filtered alignments

--- src/PacbioSubgraph.py
+++ src/PacbioSubgraph.py
@@ -12,25 +12,25 @@ class PacbioSubgraph():
         self.ThrePOvhg = []
         self.Connects = []
         #Create array of read aligments by read length
-        self.pacbio_mapping = pacbio_mapping
+        self.mapping = pacbio_mapping
         self.readArray = self.read_stack()
         qStart = 0
         qEnd = 0
         #Mark coordinates of alignments on read
-        for n in range(len(self.pacbio_mapping.readToContig[pacbio_id])):
-            qStart = self.pacbio_mapping.readToContig[pacbio_id][n].qStart
-            qEnd = self.pacbio_mapping.readToContig[pacbio_id][n].qEnd
+        for n in range(len(self.mapping.readToContig[pacbio_id])):
+            qStart = self.mapping.readToContig[pacbio_id][n].qStart
+            qEnd = self.mapping.readToContig[pacbio_id][n].qEnd
             self.mark_coords(n, qStart, qEnd)
         #Calculate per-base coverage of read
         self.covArray = self.readArray.sum(axis=0)
         #Filter out alignments in repetitive regions
         align_coords = set([])
         repeat_coords = set(self.find_repeats(cov_cutoff))
-        for n in self.pacbio_mapping.readToContig[pacbio_id]:
+        for n in self.mapping.readToContig[pacbio_id]:
             align_coords = set(range(n.qStart, n.qEnd))
             self.filter_repeats(align_coords, repeat_coords, n)
         #Find 5' and 3' overhangs
-        for n in self.pacbio_mapping.readToContig[pacbio_id]:
+        for n in self.mapping.readToContig[pacbio_id]:
             self.find_overhangs(n)
         #Connect 5' and 3' overhangs
         possible_connects = list(itertools.product(self.ThrePOvhg, self.FivePOvhg))
@@ -38,8 +38,8 @@ class PacbioSubgraph():
         self.Connects = [x for x in self.Connects if x is not None]
     
     def read_stack(self):
-        stack_size = len(self.pacbio_mapping.readToContig[self.pacbio_id])
-        read_size = self.pacbio_mapping.readToContig[self.pacbio_id][0].qLength
+        stack_size = len(self.mapping.readToContig[self.pacbio_id])
+        read_size = self.mapping.readToContig[self.pacbio_id][0].qLength
         a = np.zeros((stack_size, read_size))
 #        print "Shape of array:", np.shape(a)
         return a
@@ -58,9 +58,8 @@ class PacbioSubgraph():
 
     def filter_repeats(self, align_coords, repeat_coords, n, fraction=0.5):
         bp_in_repeat = align_coords.intersection(repeat_coords)
-        alignment = self.pacbio_mapping.readToContig[self.pacbio_id]
-        if len(bp_in_repeat) >= fraction*len(align_coords):
-            alignment = alignment.remove(n)
+        if len(bp_in_repeat) >= (fraction * len(align_coords)):
+            self.mapping.readToContig[self.pacbio_id] = self.mapping.readToContig[self.pacbio_id].remove(n)
 
     def find_overhangs(self, a):
         if a.qStart > a.tStart and (a.qLength - a.qEnd) < (a.tLength - a.tEnd):
