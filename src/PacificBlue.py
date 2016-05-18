66ddc87dfbfdec7d4134006e80c0603aef60fab2
dbrowneup <dbrowne.up@gmail.com>
2016-05-18 14:08:23 -0500

    Added in parameter controls to PacificBlue.py

--- src/PacificBlue.py
+++ src/PacificBlue.py
@@ -13,17 +13,20 @@
 
 #Load modules
 
+import sys
+import argparse
+from datetime import datetime
+from multiprocessing import Pool
+from math import ceil
+
+import matplotlib
+import matplotlib.pyplot as plt
+
 from PacbioMapping import PacbioMapping
 from PacbioSubgraph import PacbioSubgraph
 from ScaffoldGraph import ScaffoldGraph
 from PathFinder import PathFinder
-from datetime import datetime
-from multiprocessing import Pool
-from math import ceil
-import matplotlib
-import matplotlib.pyplot as plt
-import sys
-import argparse
+
 
 #Parse command-line arguments
 
@@ -34,6 +37,9 @@ parser.add_argument('--blasr-format', help="Format of BLASR alignments", dest='b
 parser.add_argument('--fasta', help="Fasta sequences to scaffold", dest='fasta_file', metavar='FILE')
 parser.add_argument('--threads', help="Number of CPUs to use", dest='num_threads', metavar='NN', type=int, default=1)
 parser.add_argument('--output', help="Name of output file", dest='output_file', metavar='FILE')
+parser.add_argument('--cov_cutoff', help="PacbioSubgraph coverage cutoff (default=3)", dest='cov_cutoff', metavar='NN', type=int, default=3)
+parser.add_argument('--fraction', help="PacbioSubgraph repeat fraction (default=0.5)", dest='fraction', metavar='(0,1]', type=float, default=0.5)
+parser.add_argument('--edge_cutoff', help="ScaffoldGraph edge cutoff (default=0.25)", dest='edge_cutoff', metavar='(0,1]', type=float, default=0.25)
 
 args = parser.parse_args()
 
@@ -42,7 +48,7 @@ args = parser.parse_args()
 
 def ParallelSubgraph(item):
     read, mapping = item
-    sg = PacbioSubgraph(read, mapping)
+    sg = PacbioSubgraph(read, mapping, cov_cutoff=args.cov_cutoff, fraction=args.fraction)
     if len(sg.Connects) == 0:
         return
     n = len(sg.Connects)
@@ -71,7 +77,7 @@ def main():
     connection_lists = [clist for clist in connection_lists if clist is not None]
     print "Leaving parallel PacbioSubgraph module:", str(datetime.now())
     #Load reported connections in ScaffoldGraph object
-    scaff = ScaffoldGraph(args.fasta_file, connection_lists)
+    scaff = ScaffoldGraph(args.fasta_file, connection_lists, edge_cutoff=args.edge_cutoff)
     #Build scaffold sequences with PathFinder object
     paths = PathFinder(scaff.graph)
     #Write output to Fasta file
@@ -80,6 +86,7 @@ def main():
         out.write(">scaffold_"+str(i+1))
         out.write(path.scaffolds[i])
     out.close()
+
 
 if __name__== '__main__':
     main()
