9643aa213bbe4a69750562366091647831b57aa3
dbrowneup <dbrowne.up@gmail.com>
2016-05-18 21:14:13 -0500

    Updated and improved edge filtration in ScaffoldGraph

--- src/ScaffoldGraph.py
+++ src/ScaffoldGraph.py
@@ -1,4 +1,4 @@
-#Re-written by Dan Browne on 04/29/16
+#Written by Dan Browne
 
 from pyfaidx import Fasta
 from datetime import datetime
@@ -22,6 +22,7 @@ class ScaffoldGraph():
             self.graph.add_node(-1 * int(x.name), seq=self.revc(str(x)))
         print "Nodes in graph:", len(self.graph.nodes())
         #Parse connections into graph as edges
+        print "Adding edges to graph"
         for clist in ConnectionLists:
             for connect in clist:
                 v1, v2, d, n = connect
@@ -33,19 +34,31 @@ class ScaffoldGraph():
         for v1, v2 in self.graph.edges():
             attr = zip(self.graph[v1][v2]['weights'], self.graph[v1][v2]['dist_estimates'])
             self.test_edge(v1, v2, attr, edge_cutoff)
-        print "Number of edges after noisy edge removal:", len(self.graph.edges())
+        print "Number of edges remaining:", len(self.graph.edges())
+        self.degree_counter()
+        #Filter self-loop edges
+        print "Removing self-loop edges"
+        selfloops = self.graph.selfloop_edges()
+        self.graph.remove_edges_from(selfloops)
+        print "Number of edges remaining:", len(self.graph.edges())
+        self.degree_counter()
+        #Filter edges leading to tips
+        print "Removing tip edges"
+        tip_edges = self.find_tips()
+        self.graph.remove_edges_from(tip_edges)
+        print "Number of edges remaining:", len(self.graph.edges())
         self.degree_counter()
         #Filter weak edges from noisy nodes
         print "Removing weak edges"
-        for n in self.graph:
-            self.weak_edges(n)
-        print "Number of edges after weak edge removal:", len(self.graph.edges())
+        weak_edges = self.find_weak_edges()
+        self.graph.remove_edges_from(weak_edges)
+        print "Number of edges remaining:", len(self.graph.edges())
         self.degree_counter()
         #Filter edges from branching nodes
         print "Removing branching edges"
-        for n in self.graph:
-            self.filter_branches(n)
-        print "Number of edges after branch removal:", len(self.graph.edges())
+        branching_edges = self.find_branches()
+        self.graph.remove_edges_from(branching_edges)
+        print "Number of edges remaining:", len(self.graph.edges())
         self.degree_counter()
         print "Leaving ScaffoldGraph module:", str(datetime.now())
     
@@ -61,15 +74,19 @@ class ScaffoldGraph():
             self.graph.add_edge(v1, v2, {'weights': [float(1) / n], 'dist_estimates': [d]})
 
     def degree_counter(self):
-        deg = self.graph.degree().values()
-        deg0 = deg.count(0)
-        deg1 = deg.count(1)
-        deg2 = deg.count(2)
-        deg3 = deg.count(3)
-        deg4 = deg.count(4)
-        deg5 = len(deg) - (deg0 + deg1 + deg2 + deg3 + deg4)
+        deg = self.graph.in_degree().values()
+        in_deg = [deg.count(n) for n in range(5)]
+        plus = len(deg) - sum(in_deg)
+        in_deg.append(plus)
+        in_deg.append(max(deg))
+        deg = self.graph.out_degree().values()
+        out_deg = [deg.count(n) for n in range(5)]
+        plus = len(deg) - sum(out_deg)
+        out_deg.append(plus)
+        out_deg.append(max(deg))
         print "Degree Frequency:"
-        print "0:{0} 1:{1} 2:{2} 3:{3} 4:{4} 5+:{5}".format(deg0, deg1, deg2, deg3, deg4, deg5)
+        print "IN 0:{0} 1:{1} 2:{2} 3:{3} 4:{4} 5+:{5} Max:{6}".format(*in_deg)
+        print "OUT 0:{0} 1:{1} 2:{2} 3:{3} 4:{4} 5+:{5} Max:{6}".format(*out_deg)
     
     def test_edge(self, v1, v2, attr, edge_cutoff):
         credible_attr = []
@@ -90,21 +107,37 @@ class ScaffoldGraph():
             self.graph[v1][v2]['weights'] = sum(wt)
             self.graph[v1][v2]['dist_estimates'] = de.mean()
     
-    def weak_edges(self, n):
-        in_wt = np.array([self.graph[v1][v2]['weights'] for v1, v2 in self.graph.in_edges(n)])
-        out_wt = np.array([self.graph[v1][v2]['weights'] for v1, v2 in self.graph.out_edges(n)])
-        for v1, v2 in self.graph.in_edges(n):
-            e_wt = self.graph[v1][v2]['weights']
-            if e_wt < in_wt.mean():
-                self.graph.remove_edge(v1, v2)
-        for v1, v2 in self.graph.out_edges(n):
-            e_wt = self.graph[v1][v2]['weights']
-            if e_wt < out_wt.mean():
-                self.graph.remove_edge(v1, v2)
+    def find_weak_edges(self):
+        weak_edges = []
+        for n in self.graph.nodes():
+            in_wt = np.array([self.graph[v1][v2]['weights'] for v1, v2 in self.graph.in_edges(n)])
+            out_wt = np.array([self.graph[v1][v2]['weights'] for v1, v2 in self.graph.out_edges(n)])
+            for v1, v2 in self.graph.in_edges(n):
+                e_wt = self.graph[v1][v2]['weights']
+                if e_wt < in_wt.mean():
+                    weak_edges.append((v1, v2))
+            for v1, v2 in self.graph.out_edges(n):
+                e_wt = self.graph[v1][v2]['weights']
+                if e_wt < out_wt.mean():
+                    weak.edges.append((v1, v2))
+        return weak_edges
     
-    def filter_branches(self, n):
-        if len(self.graph.in_edges(n)) > 1:
-            self.graph.remove_edges_from(self.graph.in_edges(n))
-        if len(self.graph.out_edges(n)) > 1:
-            self.graph.remove_edges_from(self.graph.out_edges(n))
+    def find_tips(self):
+        tip_edges = []
+        for v1, v2 in self.graph.edges():
+            v1_out = self.graph.out_degree(v1)
+            v2_in = self.graph.in_degree(v2)
+            v2_out = self.graph.out_degree(v2)
+            if v1_out > 1 and v2_in == 1 and v2_out == 0:
+                tip_edges.append((v1, v2))
+        return tip_edges
+
+    def find_branches(self):
+        branching_edges = []
+        for n in self.graph.nodes():
+            if len(self.graph.in_edges(n)) > 1:
+                branching_edges += self.graph.in_edges(n)
+            if len(self.graph.out_edges(n)) > 1:
+                branching_edges += self.graph.out_edges(n)
+        return branching_edges
 
